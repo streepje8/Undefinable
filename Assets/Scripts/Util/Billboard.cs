642e3c7bae0278d38735f97d7c1c8575227146ba
streepje8 <wessel@wezzel.nl>
2022-06-08 15:50:06 +0200

    Billboard rotation locking

--- Assets/Scripts/Util/Billboard.cs
+++ Assets/Scripts/Util/Billboard.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class Billboard : MonoBehaviour
 {
+    public BillboardSettings constraints;
     void Update()
     {
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(-(Camera.main.transform.position - transform.position).normalized,Vector3.up), 10f * Time.deltaTime);
+        Vector3 rotationthing = Quaternion.LookRotation(-(Camera.main.transform.position - transform.position).normalized, Vector3.up).eulerAngles;
+        rotationthing.x *= constraints.lockX ? 0 : 1;
+        rotationthing.y *= constraints.lockY ? 0 : 1;
+        rotationthing.z *= constraints.lockZ ? 0 : 1;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(rotationthing), 10f * Time.deltaTime);
+    }
+
+    [System.Serializable]
+    public struct BillboardSettings
+    {
+        public bool lockX;
+        public bool lockY;
+        public bool lockZ;
     }
 }
+
