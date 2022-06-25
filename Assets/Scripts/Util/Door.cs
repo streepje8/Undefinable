748467535c60ea7a533db2e8ff194f6ea46deba8
streepje8 <wessel@wezzel.nl>
2022-06-25 16:56:21 +0200

    Made the doors work and some events

---
+++ Assets/Scripts/Util/Door.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public Vector3 rotationOffset = Vector3.zero;
+    public bool interactable = true;
+    private int rotation = 0;
+    public void ToggleOpenClosed()
+    {
+        if (interactable)
+            rotation = rotation < 10 ? 145 : 0;
+    }
+
+    public void SetDoorState(bool state)
+    {
+        rotation = state ? 145 : 0;
+    }
+
+    void Update()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(rotationOffset + Vector3.up * rotation), 3f * Time.deltaTime);
+    }
+}
